09a26b05ec34c0333240c97d02fa95b636d242c3
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-01-14 19:44:24 -0800

    App.js

---
+++ public/src/App.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+function App() {
+  return <h2>Bday Reminder Project</h2>;
+}
+
+export default App;
