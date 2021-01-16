73454bb7e6d22175006a1fa0795da06f77263cae
Stephanie Raymos <stephanieraymos@gmail.com>
2021-01-15 17:52:07 -0800

    Setting up

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import List from './List';
 function App() {
   return (
     <>
-
+      <main>
+        <section className="container">
+          <h3>0 birthdays today</h3>
+        </section>
+      </main>
     </>
   );
 }
